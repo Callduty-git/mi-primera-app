7536d2159e499b1fb93f65efb1ede3d212e0c933
Juan Zabala <juanpablozabla@gmail.com>
2025-04-25 11:23:19 -0500

    colores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import escudosena from '/Escudosena.png'
 import senaLogo from '/Logosena.png'
 import './App.css'
 import Saludo from './components/saludo'; // Importa el componente
@@ -13,37 +12,38 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://static.wikia.nocookie.net/logopedia/images/e/eb/Escudosena.png/revision/latest?cb=20240403134229&path-prefix=es" target="_blank">
-          <img src={escudosena} className="logo react" alt="React logo" />
-        </a>
+      <div className='border- && flex flex-col items-center justify-center min-h-screen py-2'>
         <a href="https://c0.klipartz.com/pngpicture/440/795/gratis-png-logo-servicio-nacional-de-aprendizaje-sena-simbolo-servicio-de-entrenamiento-nacional-logo-de-dragon-ball-thumbnail.png" target="_blank">
-          <img src={senaLogo} className="logo" alt="Vite logo" />
+          <img src={senaLogo} className="" alt="Vite logo" />
         </a>
-      </div>
-      <h1>Mi primera app</h1>
-          <div>
+     
+
+      <h1 className='text-purple-500 && font-mono && font-bold && text-6xl && text-center '>Mi primera app</h1>
+          <div className="text-purple-500 && font-mono && font-bold && text-6xl && text-center pt-20 ">
       {/* Usa el componente como una etiqueta HTML */}
-      <Saludo nombre="Pepito" />
+      <div><Saludo nombre="Pepito" />
+      </div>
 
     </div>
-      <p>Edit <code>src/App.jsx</code> </p>{count}
       
-      <div className="card">
+      <div className="text-purple-500 && font-mono && font-bold && text-6xl && text-center pt-20">
+      {count}
+            </div>
+      <div className="relative border-8 border-white && border-dashed && text-red-500 && font-mono && font-bold && text-6xl && text-center mr-200  ml-200  m-20">
         <button onClick={() => setCount(count - 1)}>
           Decrementar 
         </button>
       </div>
 
-      <div className="card1">
+      <div className="relative border-8 border-white && border-dashed && text-green-500 && font-mono && font-bold && text-6xl && text-center mr-200  ml-200  m-20 ">
         <button onClick={() => setCount(count + 1)}>
           Incrementar 
         </button>
       </div>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="text-yellow-500 && font-mono && font-bold && text-6xl && text-center pt-20  ">
+        solo sena
+      </p> </div>
     </>
   )
 }
